bed3dc27f648e83ed8bf275ed71b6b680c460bc4
guozh720 <1041674298@qq.com>
2022-09-18 23:13:39 +0800

    change to get request

--- src/main/java/com/guozh/personal_blog_backend/controller/ArticleController.java
+++ src/main/java/com/guozh/personal_blog_backend/controller/ArticleController.java
@@ -3,6 +3,8 @@ package com.guozh.personal_blog_backend.controller;
 import com.guozh.personal_blog_backend.entity.Article;
 import com.guozh.personal_blog_backend.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +12,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("article")
+@CrossOrigin
 public class ArticleController {
 
     @Autowired
     private ArticleService articleService;
 
-    @RequestMapping("/listAll")
+    @GetMapping("/listAll")
     public List<Article> listAll() {
         return articleService.listAll();
     }
